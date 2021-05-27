d58c273f5f5d501c4c71c55f528ed91f462f6720
Joern Auerbach <joern.auerbach@informatik.hs-fulda.de>
2021-05-27 16:21:34 +0200

    Add Uebung 05 Aufgabe 02

---
+++ Mob_Programming/Gruppe2/Uebung05/Aufgabe02/ueb05a02.js
@@ -0,0 +1,117 @@
+let members = [
+  {
+    name: "Smith",
+    vorname: "John",
+    email: "foo@bar.com",
+  },
+  {
+    name: "Doe",
+    vorname: "Jane",
+    email: "jane@doe.org",
+  },
+  {
+    name: "Jung",
+    vorname: "Yvonne",
+    email: "jung@yvonne.io",
+  },
+  {
+    name: "Achmedow",
+    vorname: "Nidzad",
+    email: "nidz@dachme.dow",
+  },
+  {
+    name: "Jörn",
+    vorname: "Auerbach",
+    email: "dont@mail.me",
+  },
+];
+
+// using Set to store unique values for exercise 2
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+const groupSet = new Set();
+
+const table = document.getElementById("myTable");
+const ul = document.getElementById("myBullet");
+
+const createTable = () => {
+  createTableHeader();
+  createTableRows();
+}
+
+const createTableHeader = () => {
+  const thead = document.createElement("thead");
+  const tr = document.createElement("tr");
+
+  // assumption: all objects in members have the same attributes
+  for (key in members[0]) {
+    const th = document.createElement("th");
+    th.innerHTML = key;
+    tr.append(th);
+  }
+
+  const group = document.createElement("th");
+  group.innerHTML = "gruppe";
+  tr.append(group);
+
+  thead.append(tr);
+  table.append(thead);
+};
+
+const createTableRows = () => {
+  const tbody = document.createElement("tbody");
+  for (let i = 0; i < members.length; i++) {
+    const tr = createTableRow(members[i]);
+    tbody.append(tr)
+  }
+  table.append(tbody);
+};
+
+const createTableRow = (member) => {
+  const tr = document.createElement("tr");
+
+  // create table cells with data of members
+  for (key in member) {
+    const td = document.createElement("td");
+    td.innerHTML = member[key];
+    tr.append(td);
+  }
+
+  // create table cell with buttons
+  const td = createButtons(member);
+  tr.append(td);
+
+  return tr;
+};
+
+const createButtons = (member) => {
+  const td = document.createElement("td");
+  const buttonAdd = document.createElement("button");
+  const buttonRemove = document.createElement("button");
+
+  buttonAdd.innerHTML = "Add";
+  buttonAdd.onclick = function() {
+    groupSet.add(member);
+    refreshGroupList();
+  }
+  buttonRemove.innerHTML = "Remove";
+  buttonRemove.onclick = function() {
+    groupSet.delete(member);
+    refreshGroupList();
+  }
+  td.append(buttonAdd, buttonRemove);
+
+  return td;
+}
+
+const refreshGroupList = () => {
+  ul.innerHTML = "";
+  for (let member of groupSet) {
+    const li = document.createElement("li");
+    li.innerHTML = `${member.vorname} ${member.name}`
+    ul.append(li)
+  }
+}
+
+createTable();
+
+
